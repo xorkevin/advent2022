db2ba4fb0df664f28c710105a2b4ea9740cb582b
Kevin Wang <kevin@xorkevin.com>
2022-11-30 22:19:10 -0800

    Initial commit

---
+++ day01/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+const (
+	puzzleInput = "input.txt"
+)
+
+func main() {
+	file, err := os.Open(puzzleInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	var weights []int
+	current := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			weights = append(weights, current)
+			current = 0
+			continue
+		}
+
+		num, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+		current += num
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	weights = append(weights, current)
+	sort.Ints(weights)
+	if len(weights) == 0 {
+		return
+	}
+	last := len(weights) - 1
+	fmt.Println("Part 1:", weights[last])
+	if last < 2 {
+		return
+	}
+	fmt.Println("Part 2:", weights[last]+weights[last-1]+weights[last-2])
+}
