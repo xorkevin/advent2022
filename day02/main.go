8d1192452621618300baafae28484e44ce0fb56d
Kevin Wang <kevin@xorkevin.com>
2022-12-01 22:19:22 -0800

    Finish day02

---
+++ day02/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+const (
+	puzzleInput = "input.txt"
+)
+
+func main() {
+	file, err := os.Open(puzzleInput)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	score1 := 0
+	score2 := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		games := strings.Fields(scanner.Text())
+		if len(games) != 2 {
+			log.Fatalln("Invalid line")
+		}
+
+		theirMove := toMove(games[0])
+		ownMove := toMove(games[1])
+
+		score1 += winner(theirMove, ownMove) + ownMove + 1
+		score2 += ownMove*3 + pickMove(theirMove, ownMove) + 1
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Part 1:", score1)
+	fmt.Println("Part 2:", score2)
+}
+
+func pickMove(a, b int) int {
+	return (a + b + 2) % 3
+}
+
+func winner(a, b int) int {
+	switch (b - a + 3) % 3 {
+	case 0:
+		return 3
+	case 1:
+		return 6
+	case 2:
+		return 0
+	default:
+		log.Fatalln("Invalid match")
+		return 0
+	}
+}
+
+func toMove(k string) int {
+	switch k {
+	case "A", "X":
+		return 0
+	case "B", "Y":
+		return 1
+	case "C", "Z":
+		return 2
+	default:
+		log.Fatalln("Invalid move")
+		return 0
+	}
+}
